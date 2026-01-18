024dfb658fa5385fbef282a2380a1dda3b7330d6
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2026-01-18 15:29:55 +0300

    feat: Check if username and email exist before adding a new user.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, send_from_directory, jsonify, request
 import sqlite3, bcrypt
 
+# Constants
+USERS_DATABASE = "users.db"
+
 app = Flask(__name__, static_folder="static", static_url_path="")
 
 def init_db():
@@ -28,12 +31,28 @@ def index():
 @app.route("/signup", methods=["POST"])
 def signup():
     data = request.get_json()
-    password_hash = bcrypt.hashpw(data["password"].encode("utf-8"), bcrypt.gensalt())
+    username = data["username"]
+    email = data["email"]
+    password = data["password"]
 
-    conn = sqlite3.connect("users.db")
+    # Check if username or email exists.
+    conn = sqlite3.connect(USERS_DATABASE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+    if cursor.fetchone():
+        conn.close()
+        return "Username already exists."
+
+    cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
+    if cursor.fetchone():
+        conn.close()
+        return "Email already exists."
+
+    # Add user.
+    password_hash = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     conn.execute(
         "INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)",
-        (data["username"], data["email"], password_hash)
+        (username, email, password_hash)
     )
     conn.commit()
     conn.close()
